f41f3236bb3bda5b3772c39b6c52a0f684ece0d2
max-de-rooij <54850292+max-de-rooij@users.noreply.github.com>
2023-08-23 13:40:43 +0200

    Add Dynamic Modelling & Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,4 +14,17 @@ end
   @test test_nb("../01-Introduction/02-Working-With-Julia.ipynb")
 end
 
+@testset "02 - Machine Learning: Working with Flux" begin 
+  @test test_nb("../02-Machine-Learning/01-Working-with-Flux.ipynb")
+end
+
+@testset "03 - Dynamic Modeling: ODEs" begin 
+  @test test_nb("../03-Dynamic-Modeling/01-ODEs-in-Julia.ipynb")
+end
+
+@testset "03 - Dynamic Modeling: Parameter Estimation" begin 
+  @test test_nb("../03-Dynamic-Modeling/02-Parameter-Estimation.ipynb")
+end
+
+
 
